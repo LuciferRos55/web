31dd3934e715421696825b16cfe029246c0f0cdc
LuciferRos55 <minhchau186@gmail.com>
2022-04-20 01:58:19 +0700

    test suite test

---
+++ Test Suites/Login test suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login test suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1d598ffa-0df5-46f2-abc5-5c0a7fbb2856</testSuiteGuid>
+</TestSuiteEntity>
