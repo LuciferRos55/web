4b9f6d794c5822665a7e850f305a165e173a0170
LuciferRos55 <minhchau186@gmail.com>
2022-04-20 18:48:47 +0700

    test suites

---
+++ Test Suites/Register test suite.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Register test suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>886e1909-a217-4564-bfc9-1651ffe3d5b8</testSuiteGuid>
+   <testCaseLink>
+      <guid>5e1de876-e0aa-45b9-981e-47a17d2e48a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_register</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f6a59cdb-2958-4c2e-a3d3-76936c188dc7</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/TD_Login</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>f6a59cdb-2958-4c2e-a3d3-76936c188dc7</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>mail2</value>
+         <variableId>44a50bd2-33a7-42f7-85dc-587415b9569f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f6a59cdb-2958-4c2e-a3d3-76936c188dc7</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password2</value>
+         <variableId>6667199b-6aa3-49f0-a671-7dc40c1d1651</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f6a59cdb-2958-4c2e-a3d3-76936c188dc7</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>reinput</value>
+         <variableId>a2c54501-064f-4f4f-801f-c9d9baf874f1</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
